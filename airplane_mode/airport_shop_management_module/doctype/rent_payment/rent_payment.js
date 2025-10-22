1b1f3d9ca4839ce2c67b01eca65b953e1ad9c6ab
antony-joy-sathish <antonyjoysathish@dbcyelagiri.edu.in>
2025-10-22 19:52:02 +0530

    Payment Print Formate

--- airplane_mode/airport_shop_management_module/doctype/rent_payment/rent_payment.js
+++ airplane_mode/airport_shop_management_module/doctype/rent_payment/rent_payment.js
@@ -1,8 +1,28 @@
 // Copyright (c) 2025, deepak and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Rent Payment", {
-// 	refresh(frm) {
+frappe.ui.form.on("Rent Payment", {
+    // Trigger when cost or amount_paid changes
+    cost: function(frm) {
+        calculate_remaining(frm);
+    },
 
-// 	},
-// });
+    amount_paid: function(frm) {
+        calculate_remaining(frm);
+    }
+});
+
+// Helper function
+function calculate_remaining(frm) {
+    const cost = frm.doc.cost || 0;
+    const amount_paid = frm.doc.amount_paid || 0;
+    const remaining = cost - amount_paid;
+
+    frm.set_value('remaining', remaining);
+    if (remaining == 0){
+        frm.set_value("payment_status", "Paid")
+    }else{
+        frm.set_value("payment_status", "Pending")
+    }
+}
+
--- airplane_mode/airport_shop_management_module/doctype/shop/shop.py
+++ airplane_mode/airport_shop_management_module/doctype/shop/shop.py
@@ -1,9 +1,23 @@
 # Copyright (c) 2025, deepak and contributors
 # For license information, please see license.txt
 
-# import frappe
-from frappe.model.document import Document
+import frappe
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class Shop(Document):
-	pass
+class Shop(WebsiteGenerator):
+	def before_save(self):
+		self.route = f"shops/{self.name.lower().replace(' ','-')}"
+
+	def validate(self):
+		# Validate lease dates
+		if self.lease_start_date and self.lease_expiry_date:
+			if self.lease_start_date >= self.lease_expiry_date:
+				frappe.throw("Lease Expiry Date must be greater than Lease Start Date")
+
+		# Validate rent amount
+		shop_config = frappe.get_single("Shop Configuration")
+		default_rent = shop_config.default_rent
+
+		if self.rent_amount is not None and self.rent_amount < default_rent:
+			frappe.throw(f"Rent amount must be greater than or equal to the default rent: {default_rent}")
